9f0d25a2a6b958bd4b6c1d5b3117d23e5d84be2a
sp1rexxnxxsp1rexxnxx <41951443+sp1rexxnxxsp1rexxnxx@users.noreply.github.com>
2018-08-26 11:15:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const client = new Discord.Client();
 const bot = new Discord.Client({disableEveryone: true})
-bot.on("message", async message => {
+clint.on("message", async message => {
     if (message.author.bot) return;
     if (message.channel.type === "dm") return;
     if (!message.member.voiceChannel) return message.channel.send('I can\'t find u in any voice channel')
